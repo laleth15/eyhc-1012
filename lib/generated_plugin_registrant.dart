0043c1a251db59dd1c980a0de1567581717b60dd
laleth15 <63454572+laleth15@users.noreply.github.com>
2021-05-23 16:58:38 +0530

    Add files via upload

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,12 +4,12 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  UrlLauncherPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
